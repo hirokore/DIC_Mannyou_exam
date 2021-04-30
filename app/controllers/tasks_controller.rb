4c663dab836d166c2c6ec13e3845753860d04854
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-04-30 16:11:39 +0900

    update: タスクの一覧画面、詳細画面を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :detail)
+    params.require(:task).permit(:name, :details)
   end
 
   def set_task
