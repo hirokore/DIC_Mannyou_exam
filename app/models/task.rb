362e8b3829ba45624088332524be3721be3aedf4
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-01 21:29:43 +0900

    update: model spec記載及びvalidates追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
+  validates :details, presence: true, length: {maximum: 1000}
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :task do
-    name { "test_title" }
-    details { "testdtails" }
+    
   end
 end
