40853a9988399777c5bfc075a742c9836f9b3f85
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-03 01:18:53 +0900

    update: basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_authentication
+
+  private
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |name, password|
+      name == "dic" && password == "P@ssw0rd!"
+    end
+  end
 end
