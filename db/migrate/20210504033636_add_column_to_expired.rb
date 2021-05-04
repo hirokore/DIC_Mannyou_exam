248a457f83c2294ead6bc422c2950b3c9d226b09
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-04 22:02:29 +0900

    update: expired_atカラム追加null/default

---
+++ db/migrate/20210504033636_add_column_to_expired.rb
@@ -0,0 +1,9 @@
+class AddColumnToExpired < ActiveRecord::Migration[5.2]
+  def up
+    add_column :tasks, :expired_at, :date, default: "2030-12-31", null: false
+
+  end
+  def down
+    remove_column :tasks, :expired_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_034848) do
+ActiveRecord::Schema.define(version: 2021_05_04_033636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_04_30_034848) do
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "expired_at", default: "2030-12-31", null: false
   end
 
 end
