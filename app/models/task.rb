c69ee30fa9c02201235c08def679b65d396a7f22
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-06 23:03:15 +0900

    update: 優先順位用のカラム追加及び、enumの設定追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   validates :details, presence: true, length: {maximum: 1000}
   enum status: {"---":0, 未着手:1, 着手中:2, 完了:3}
+  enum priority: {"---":0, 低:1, 中:2, 高:3}, _prefix: true
 
   include Searchable
   def_name_status_like :name, :status
