c49fa24cd6bb2376ff85692fa5f8bc2ffcebdfae
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-07 23:06:53 +0900

    update: herokuエラー対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_authentication if Rails.env.production?
 
-  private
-
-  def basic_authentication
-    authenticate_or_request_with_http_basic do |name, password|
-      name == "dic" && password == "P@ssw0rd!"
-    end
-  end
 end
