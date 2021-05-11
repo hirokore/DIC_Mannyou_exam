d724aaaf076eea466d6823828937f3de28010b49
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-11 20:14:14 +0900

    update: ユーザー作成後にログインできるよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id), notice: "ユーザが作成されました！"
     else
       render :new
@@ -25,7 +26,7 @@ class UsersController < ApplicationController
 
   def show
     @users = User.find(params[:id])
-    redirect_to blogs_path, notice: "不正操作を記録しました。" unless current_user.id == @users.id
+    redirect_to tasks_path, notice: "不正操作を記録しました。" unless current_user.id == @users.id
   end
 
   def edit
