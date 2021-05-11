faa43f5e601b617c495db18d8075fb01960e1f73
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-11 11:23:16 +0900

    update: セキュアパスワード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  has_secure_password
   before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 128 }
   validates :email, presence: true, length: { maximum: 255 },format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
