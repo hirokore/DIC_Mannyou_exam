7c89ac1a27fb980dda621ead0d092984da82c0c1
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-12 01:06:56 +0900

    update: 管理者権限判断用カラム作成、header表示変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_014625) do
+ActiveRecord::Schema.define(version: 2021_05_11_155946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_014625) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "tasks", "users"
