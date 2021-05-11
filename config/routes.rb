bcf8e9c5dc5ec6ea3850fa468a6ddcf925560179
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-11 22:43:25 +0900

    update: 管理者用画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :tasks
+  namespace :admin do
+    resources :users
+  end
 end
