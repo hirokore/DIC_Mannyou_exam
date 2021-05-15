d78576cb31103c10ca5ac232c22f5bb816559117
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-05-15 16:20:41 +0900

    update: ラベル作成機能作成

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -2,4 +2,28 @@ class LabelsController < ApplicationController
   def new
     @label = Label.new
   end
+
+  def create
+    @label = Label.new(label_params)
+    @label.user_id = current_user.id
+    if @label.save
+      redirect_to user_path(current_user), notice: "登録完了"
+    else
+      render :new
+    end
+  end
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
+
+  def set_label
+    @label = Label.find(params[:id])
+  end
+
+  def irregul_operation
+    redirect_to labels_path, notice: "不正操作を記録しました。" unless current_user.id == @label.user.id
+  end
+
 end
